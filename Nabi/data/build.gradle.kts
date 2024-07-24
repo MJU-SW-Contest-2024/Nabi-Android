ab347b08acb20e272828b1597bbcdf0ef4a40d8c
HamBeomJoon <hbj0802@naver.com>
2024-07-24 15:43:03 +0900

    [setting] build.gradle 설정

--- Nabi/data/build.gradle.kts
+++ Nabi/data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -34,10 +35,30 @@ android {
 
 dependencies {
 
+    implementation(project(":domain"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // LoggingInterceptor
+    implementation(libs.logging.interceptor)
+    implementation(libs.okhttp)
+
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
+
+    // LoggerUtils
+    implementation(libs.logger)
 }
