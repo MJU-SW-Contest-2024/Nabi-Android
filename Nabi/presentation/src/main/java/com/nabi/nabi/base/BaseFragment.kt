beb7988a8648f8543fa8c464c778fb1cad344fee
moondev03 <moondev03@gmail.com>
2024-07-27 00:43:41 +0900

    [feat] Add BaseActivity, BaseFragment

---
+++ Nabi/presentation/src/main/java/com/nabi/nabi/base/BaseFragment.kt
@@ -0,0 +1,52 @@
+package com.nabi.nabi.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.nabi.data.utils.LoggerUtils
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutId : Int): Fragment() {
+    private var _binding : T? = null
+    protected val binding get() = _binding!!
+
+    private var currentToast: Toast? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initView()
+        initListener()
+        setObserver()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    protected abstract fun initView()
+
+    protected open fun initListener() {}
+
+    protected open fun setObserver() {}
+
+    protected fun showToast(msg: String) {
+        currentToast?.cancel()
+        currentToast = Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT)
+        currentToast?.show()
+    }
+}
