354ac3c9d34bf7ef0ebd2312178a4b3243242c79
HamBeomJoon <hbj0802@naver.com>
2024-07-28 17:54:45 +0900

    [feat] Main페이지 작업

---
+++ Nabi/presentation/src/main/java/com/nabi/nabi/view/home/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.nabi.nabi.view.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nabi.domain.model.home.HomeInfo
+import com.nabi.domain.repository.DataStoreRepository
+import com.nabi.domain.usecase.home.HomeUseCase
+import com.nabi.nabi.utils.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val homeUseCase: HomeUseCase,
+    private val dataStoreRepository: DataStoreRepository
+) : ViewModel() {
+
+    private val _homeState = MutableLiveData<UiState<HomeInfo>>(UiState.Loading)
+    val homeState: LiveData<UiState<HomeInfo>> get() = _homeState
+
+    fun fetchData() {
+        _homeState.value = UiState.Loading
+
+        viewModelScope.launch {
+            val accessTokenResult = dataStoreRepository.getAccessToken()
+            if (accessTokenResult.isSuccess) {
+                val accessToken = accessTokenResult.getOrNull().orEmpty()
+
+                homeUseCase(accessToken).onSuccess {
+                    _homeState.value = UiState.Success(it)
+                }.onFailure { e ->
+                    _homeState.value = UiState.Failure(message = e.message.toString())
+                }
+            } else {
+                _homeState.value = UiState.Failure(message = "Failed to get access token")
+            }
+        }
+    }
+}
