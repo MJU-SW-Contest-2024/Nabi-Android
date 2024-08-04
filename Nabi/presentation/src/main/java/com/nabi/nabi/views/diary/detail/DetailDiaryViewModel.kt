040ff07ba1d358d94b26e6e0aafdf4d20ea4dc5a
moondev03 <moondev03@gmail.com>
2024-08-04 12:50:36 +0900

    [feat] 일기 상세 페이지 | 북마크 추가 삭제 기능

--- Nabi/presentation/src/main/java/com/nabi/nabi/views/diary/detail/DetailDiaryViewModel.kt
+++ Nabi/presentation/src/main/java/com/nabi/nabi/views/diary/detail/DetailDiaryViewModel.kt
@@ -7,21 +7,36 @@ import androidx.lifecycle.viewModelScope
 import com.nabi.domain.model.diary.DiaryInfo
 import com.nabi.domain.model.diary.SearchDiary
 import com.nabi.domain.repository.DataStoreRepository
+import com.nabi.domain.usecase.bookmark.AddBookmarkUseCase
+import com.nabi.domain.usecase.bookmark.DeleteBookmarkUseCase
 import com.nabi.domain.usecase.diary.GetDiaryDetailUseCase
 import com.nabi.domain.usecase.diary.SearchDiaryUseCase
 import com.nabi.nabi.utils.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailDiaryViewModel @Inject constructor(
     private val getDiaryDetailUseCase: GetDiaryDetailUseCase,
+    private val addBookmarkUseCase: AddBookmarkUseCase,
+    private val deleteBookmarkUseCase: DeleteBookmarkUseCase,
     private val dataStoreRepository: DataStoreRepository
 ) : ViewModel() {
 
+    private val _isBookmarked = MutableLiveData<Boolean>(false)
+    val isBookmarked: LiveData<Boolean> get() = _isBookmarked
+
     private val _diaryState = MutableLiveData<UiState<DiaryInfo>>(UiState.Loading)
     val diaryState: LiveData<UiState<DiaryInfo>> get() = _diaryState
+
+    private val _addState = MutableLiveData<UiState<Unit>>(UiState.Loading)
+    val addState: LiveData<UiState<Unit>> get() = _addState
+
+    private val _deleteState = MutableLiveData<UiState<Unit>>(UiState.Loading)
+    val deleteState: LiveData<UiState<Unit>> get() = _deleteState
+
 
     fun fetchData(diaryId: Int) {
         _diaryState.value = UiState.Loading
@@ -31,9 +46,42 @@ class DetailDiaryViewModel @Inject constructor(
 
             getDiaryDetailUseCase(accessToken, diaryId)
                 .onSuccess {
+                    _isBookmarked.value = it.isBookmarked
                     _diaryState.value = UiState.Success(it)
                 }.onFailure { e ->
                     _diaryState.value = UiState.Failure(message = e.message.toString())
+                }
+        }
+    }
+
+    fun addBookmark(diaryId: Int){
+        _addState.value = UiState.Loading
+
+        viewModelScope.launch {
+            val accessToken = dataStoreRepository.getAccessToken().getOrNull().orEmpty()
+
+            addBookmarkUseCase(accessToken, diaryId)
+                .onSuccess {
+                    _isBookmarked.value = true
+                    _addState.value = UiState.Success(Unit)
+                }.onFailure { e ->
+                    _addState.value = UiState.Failure(message = e.message.toString())
+                }
+        }
+    }
+
+    fun deleteBookmark(diaryId: Int){
+        _deleteState.value = UiState.Loading
+
+        viewModelScope.launch {
+            val accessToken = dataStoreRepository.getAccessToken().getOrNull().orEmpty()
+
+            deleteBookmarkUseCase(accessToken, diaryId)
+                .onSuccess {
+                    _isBookmarked.value = false
+                    _deleteState.value = UiState.Success(Unit)
+                }.onFailure { e ->
+                    _deleteState.value = UiState.Failure(message = e.message.toString())
                 }
         }
     }
