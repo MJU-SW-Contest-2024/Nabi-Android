cc2e4e647876fc15ccd2e22b1ce594258156bf7b
moondev03 <moondev03@gmail.com>
2024-08-05 05:22:57 +0900

    CI/CD Test Commit

--- Nabi/presentation/build.gradle.kts
+++ Nabi/presentation/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "KAKAO_NATIVE_KEY", "\"${properties.getProperty("KAKAO_NATIVE_KEY")}\"")
