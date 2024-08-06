b68840207042686ccae80d200f40f89a8199c871
moondev03 <moondev03@gmail.com>
2024-08-07 05:28:44 +0900

    [feat] 감정 별 일기 목록 조회 UseCase 추가

---
+++ Nabi/domain/src/main/java/com/nabi/domain/usecase/emotion/SearchEmotionUseCase.kt
@@ -0,0 +1,13 @@
+package com.nabi.domain.usecase.emotion
+
+import com.nabi.domain.model.PageableInfo
+import com.nabi.domain.model.diary.DiaryInfo
+import com.nabi.domain.model.diary.SearchDiary
+import com.nabi.domain.model.emotion.EmotionStatistics
+import com.nabi.domain.repository.EmotionRepository
+
+class SearchEmotionUseCase(private val repository: EmotionRepository) {
+    suspend operator fun invoke(accessToken: String, emotion: String, page: Int, size: Int, sort: String): Result<Pair<PageableInfo, List<SearchDiary>>>{
+        return repository.searchDiaryByEmotion("Bearer $accessToken", emotion, page, size, sort)
+    }
+}
