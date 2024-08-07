0d76aed1779ebff095544769007d67a0b9ffb5a6
moondev03 <moondev03@gmail.com>
2024-08-07 18:23:51 +0900

    [feat] ContextExtension | dialogResize 개선

--- Nabi/presentation/src/main/java/com/nabi/nabi/extension/ContextExtension.kt
+++ Nabi/presentation/src/main/java/com/nabi/nabi/extension/ContextExtension.kt
@@ -4,9 +4,11 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Point
 import android.os.Build
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.WindowManager
+import kotlin.time.times
 
-fun Context.dialogResize(dialog: Dialog, width: Float, height: Float){
+fun Context.dialogResize(dialog: Dialog, width: Float, height: Float? = null){
     val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
     if (Build.VERSION.SDK_INT < 30){
@@ -18,7 +20,7 @@ fun Context.dialogResize(dialog: Dialog, width: Float, height: Float){
         val window = dialog.window
 
         val x = (size.x * width).toInt()
-        val y = (size.y * height).toInt()
+        val y = if(height == null) WRAP_CONTENT else (size.y * height).toInt()
 
         window?.setLayout(x, y)
 
@@ -27,7 +29,7 @@ fun Context.dialogResize(dialog: Dialog, width: Float, height: Float){
 
         val window = dialog.window
         val x = (rect.width() * width).toInt()
-        val y = (rect.height() * height).toInt()
+        val y = if(height == null) WRAP_CONTENT else (rect.height() * height).toInt()
 
         window?.setLayout(x, y)
     }
