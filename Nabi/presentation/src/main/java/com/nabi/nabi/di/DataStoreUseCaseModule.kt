a183e9b731842ad0cf108c739f654cc4db94162d
moondev03 <moondev03@gmail.com>
2024-08-12 05:13:18 +0900

    [feat] DataStore | Add Auth UseCase

---
+++ Nabi/presentation/src/main/java/com/nabi/nabi/di/DataStoreUseCaseModule.kt
@@ -0,0 +1,49 @@
+package com.nabi.nabi.di
+
+import com.nabi.domain.repository.DataStoreRepository
+import com.nabi.domain.usecase.datastore.GetAccessTokenUseCase
+import com.nabi.domain.usecase.datastore.GetAuthProviderUseCase
+import com.nabi.domain.usecase.datastore.GetRecentAuthProviderUseCase
+import com.nabi.domain.usecase.datastore.SaveSignInInfoUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreUseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideGetRecentAuthProviderUseCase(
+        repository: DataStoreRepository
+    ): GetRecentAuthProviderUseCase {
+        return GetRecentAuthProviderUseCase(repository = repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAuthProviderUseCase(
+        repository: DataStoreRepository
+    ): GetAuthProviderUseCase {
+        return GetAuthProviderUseCase(repository = repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAccessTokenUseCase(
+        repository: DataStoreRepository
+    ): GetAccessTokenUseCase {
+        return GetAccessTokenUseCase(repository = repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSaveSignInInfoUseCase(
+        repository: DataStoreRepository
+    ): SaveSignInInfoUseCase {
+        return SaveSignInInfoUseCase(repository = repository)
+    }
+}
