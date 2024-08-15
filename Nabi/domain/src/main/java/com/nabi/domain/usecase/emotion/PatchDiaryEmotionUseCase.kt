d307e6d21e16a33970a8db9a2ca2df4aba86d07c
moondev03 <moondev03@gmail.com>
2024-08-16 01:14:41 +0900

    [feat] 감정 수정 | UseCase 추가

---
+++ Nabi/domain/src/main/java/com/nabi/domain/usecase/emotion/PatchDiaryEmotionUseCase.kt
@@ -0,0 +1,14 @@
+package com.nabi.domain.usecase.emotion
+
+import com.nabi.domain.model.emotion.AddDiaryEmotionMsg
+import com.nabi.domain.repository.EmotionRepository
+
+class PatchDiaryEmotionUseCase(private val repository: EmotionRepository) {
+    suspend operator fun invoke(
+        accessToken: String,
+        diaryId: Int,
+        emotion: String
+    ): Result<String> {
+        return repository.patchDiaryEmotion("Bearer $accessToken", diaryId, emotion)
+    }
+}
